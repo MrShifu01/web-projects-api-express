929148d25d19200d95446c4bdef32c235baad37f
ChristianStander <stander.christian@gmail.com>
2023-05-29 09:52:09 +0200

    have to just add comments and clean up

--- frontend/src/components/AddNew.js
+++ frontend/src/components/AddNew.js
@@ -14,45 +14,34 @@ function AddNew() {
   const projects = useSelector((state) => state.projects.projects);
   const dispatch = useDispatch();
 
-  // Function to add a new project
   async function AddProject(e, title, description, URL) {
     e.preventDefault();
 
     const existingIds = projects.map((project) => project.id);
     let newId = 1;
 
-    // Generate a new unique ID for the project
     while (existingIds.includes(newId)) {
       newId++;
     }
 
-    // Check if a project with the same title already exists
     for (let i = 0; i < projects.length; i++) {
       if (title === projects[i].title) {
-        setError(true);
-        return; // Stop execution if an error occurs
+        setError(true)
       }
     }
 
     try {
-      // Make a POST request to the server to add the project
       await axios.post(`/api/?id=${newId}&title=${title}&description=${description}&URL=${URL}`);
 
-      // Fetch the updated project data from the server
       const response = await axios.get('/api');
       const data = response.data;
-
-      // Dispatch the action to update the project array in the Redux store
       dispatch(populateArray(data));
-
-      // Dispatch the action to reset the screen
       dispatch(resetScreen());
 
     } catch (error) {
-      console.log("Error:", error);
+      console.log("Error", e)
     }
 
-    // Reset the input fields after adding the project
     setTitle('');
     setDescription('');
     setURL('');
@@ -60,19 +49,19 @@ function AddNew() {
 
   return (
     <div className='flex flex-col'>
-      {/* Display error message if an error occurred */}
       {error && (
         <div className="h-30 alert alert-error shadow-lg">
           <div>
             <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
             <span>Error! Task failed successfully.</span>
-            <span>Please enter a project that doesn't already exist.</span>
+            <span>Please enter a project that doesn't already exists</span>
           </div>
         </div>
       )}
 
       <form
         className="flex flex-col gap-3 m-10"
+        type="submit"
         onSubmit={(e) => AddProject(e, title, description, URL)}
       >
         <input
